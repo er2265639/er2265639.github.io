b1d972f02dbd66d33cad9a243394e4537cdcf6aa
er2265639 <er2265639@gmail.com>
2025-01-07 21:37:37 +0800

    調整樣式

--- calendar/js/index.js
+++ calendar/js/index.js
@@ -1,14 +1,11 @@
-var month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-
-
+const date = new Date();
+const month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
 
 $(document).ready(function () {
     setMonth();
 });
 
 function setMonth() {
-    const date = new Date();
-
     $('.month p').removeClass("current");
     $('.' + month[date.getMonth()]).addClass("current");
 }

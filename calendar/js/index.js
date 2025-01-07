6a41952dbf3f8a0de1b8ad7d016a4717bded0135
er2265639 <er2265639@gmail.com>
2025-01-07 22:14:54 +0800

    新增禮拜樣式

--- calendar/js/index.js
+++ calendar/js/index.js
@@ -1,5 +1,6 @@
 const date = new Date();
 const month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+const week = ['24rem', '20rem', '16rem', '12rem', '8rem', '4rem', '0rem'];
 
 $(document).ready(function () {
     setMonth();
@@ -8,4 +9,12 @@ $(document).ready(function () {
 function setMonth() {
     $('.month p').removeClass("current");
     $('.' + month[date.getMonth()]).addClass("current");
+}
+
+function setWeek() {
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var date = new Date(year + '-' + month + '-1');
+
+    $('.day .week').css('left', week[date.getDay()]);
 }

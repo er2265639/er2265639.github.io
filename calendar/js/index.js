87093ea76108bab03d857815a8baccdf03ec27ec
er2265639 <er2265639@gmail.com>
2025-01-07 22:20:54 +0800

    test

--- calendar/js/index.js
+++ calendar/js/index.js
@@ -1,18 +1,19 @@
-const date = new Date();
-const month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-const week = ['24rem', '20rem', '16rem', '12rem', '8rem', '4rem', '0rem'];
-
 $(document).ready(function () {
     setMonth();
     setWeek();
 });
 
 function setMonth() {
+    var date = new Date();
+    var month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
     $('.month p').removeClass("current");
     $('.' + month[date.getMonth()]).addClass("current");
 }
 
 function setWeek() {
+    var date = new Date();
+    var week = ['24rem', '20rem', '16rem', '12rem', '8rem', '4rem', '0rem'];
     var year = date.getFullYear();
     var month = date.getMonth() + 1;
     var date = new Date(year + '-' + month + '-1');
